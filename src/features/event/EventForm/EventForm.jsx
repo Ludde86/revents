e3aef45d22b4e14f5a64f5d28a4620c57e485e63
Ludde86 <bslbjorn@gmail.com>
2019-09-12 10:35:02 +0200

    crud implemented

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -10,9 +10,21 @@ class EventForm extends Component {
     hostedBy: ""
   };
 
-  handleSubmit = e => {
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
+      this.setState({
+        ...this.props.selectedEvent
+      });
+    }
+  }
+
+  handleFormSubmit = e => {
     e.preventDefault();
-    this.props.createEvent(this.state);
+    if (this.state.id) {
+      this.props.updateEvent(this.state);
+    } else {
+      this.props.createEvent(this.state);
+    }
   };
 
   handleInputChange = ({ target: { name, value } }) => {
@@ -26,7 +38,7 @@ class EventForm extends Component {
     const { title, date, city, venue, hostedBy } = this.state;
     return (
       <Segment>
-        <Form onSubmit={this.handleSubmit} autoComplete='off'>
+        <Form onSubmit={this.handleFormSubmit} autoComplete='off'>
           <Form.Field>
             <label>Event Title</label>
             <input
